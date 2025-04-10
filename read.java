b2173f42fbf6203a3a8055f7d4c9c88981b69f60
Sandhya Sapkota <sandhyasapkota64@gmail.com>
2025-04-10 21:16:25 +0545

    commit

---
+++ read.java
@@ -0,0 +1,8 @@
+public class read {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test of the read.java file.");
+        
+    }
+}
