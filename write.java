5997e061dedccc4a286e9ccdb4b992a6b587b155
Sandhya Sapkota <sandhyasapkota64@gmail.com>
2025-04-10 21:22:49 +0545

    tommit

---
+++ write.java
@@ -0,0 +1,6 @@
+public class write {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test of the write.java file.");
+}
+}
